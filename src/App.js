f67c0f390d44e228bf827fde2faef575542ff049
jaypark <jaypark@gmail.com>
2019-11-12 21:35:50 +0900

    바벨을 추가하고, 웹팩설정에서 기본값들을 삭제했다. 그래도 HMR은 잘동작한다.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+class TestClass {
+  ff() {
+    console.log("ACONSOLE");
+  }
+}
+
 function App() {
   const el = document.querySelector("#testblock");
   el.innerHTML = "이렇게 하고 저장하면?";
+  console.log("ABB");
+  (new TestClass()).ff();
 }
 
 export default App;
-
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,29 @@
-const path = require('path');
-const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: {
-    app: './src/index.js'
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
+  mode: 'development',
   module: { 
     rules: [ 
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
         test: /\.(scss|css)$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {hmr: true},
-          }, 'css-loader',]
+        use: ['style-loader', 'css-loader'],
       }
     ]
   },
   plugins: [
-    new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
         template: 'index.html',
         showErrors: true
     }),
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  devtool: 'inline-source-map',
-  devServer: {
-    hot: true,
-    inline: true
-  }
+  ]
 };
